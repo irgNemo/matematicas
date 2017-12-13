f94881dfd5fb64cf2dc9e15a9b62ac9288530d2f
Israel Roman Godinez <israel.roman@cucei.udg.mx>
2017-12-13 17:47:31 -0600

    First commit

---
+++ clusteringSilouhette.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from sklearn import datasets;
+from sklearn.cluster import KMeans;
+from sklearn import metrics;
+import pandas as pd;
+import numpy as np;
+
+def main():
+	dataset = datasets.load_iris();
+	#dataset = readDataSet("D_C_all_AM.csv");
+	data = dataset.data;
+	columns = data.itemsize;
+	#data = data[:columns-2];
+	target = dataset.target;
+	print(data[1:10]);
+	#kmeans = KMeans(n_clusters=3, random_state=1).fit(data);
+	#labels = kmeans.labels_;
+	#silhouette_scores = metrics.silhouette_samples(data, labels, metric='euclidean');
+	#print (silhouette_scores);
+
+
+
+def readDataSet(filename):
+	#TODO Validar que filename tenga algo; ademas agregar excepciones
+	dataset = pd.read_csv(filename);
+	data = dataset.values;
+	target = np.asarray(list(dataset));
+	return dataWrapper(data, target);
+	
+
+class dataWrapper:
+	def __init__(self, data, target):
+		self.data = data;
+		self.target = target;
+
+
+
+if __name__ == "__main__":
+	main();
