332da2863acd00f47c63353a0d7d592289fcfe28
Israel Roman Godinez <israel.roman@cucei.udg.mx>
2018-01-09 17:06:06 -0600

    comienzo de estructuracion del programa

--- clusteringSilouhette.py
+++ clusteringSilouhette.py
@@ -8,43 +8,54 @@ import pandas as pd;
 import numpy as np;
 
 def main():
-	#dataset = datasets.load_iris();
+
 	dataset = readDataSet("D_C_all_AM.csv");
 	data = dataset.data;
+	obtainDataMatrix(dataset,['Sujeto','clase']);
 
-	# Remover columnas y convertir arreglo en tipo float
-	columns = data.shape[1] - 2; # Numero de columnas menos las dos ultimas con valores categoricos
-	data = data[..., 0:columns]; # Remover columnas con valores categoricos
-	target = dataset.target;
-	data = np.asarray(data, dtype=float);
-	print(np.nan_to_num(data));
-	data[np.isnan(data)] = np.inf;
-	print(data);
 		
 	# Impute
-	#imp = Imputer(missing_values='?', strategy='mean', axis=0);
-	#imp.fit(data);
+	#data[np.isnan(data)] = 1.7976931348623157e+108; # Se asigno este valor de manera arbitraria para que no marcara un error de validacion por valores muy grandes
+	#data = np.nan_to_num(data);
 	
 	# Clustering
 	#kmeans = KMeans(n_clusters=3, random_state=1).fit(data);
 	#labels = kmeans.labels_;
 	#silhouette_scores = metrics.silhouette_samples(data, labels, metric='euclidean');
-	#print (silhouette_scores);
 
 
+def obtainDataMatrix(dataset, headerNameToRemove):
+	headers = dataset.header;
+	data = dataset.data;
+	arrayToRemove = list();
+	for toRemove in headerNameToRemove:
+		arrayToRemove.append(np.where(headers == toRemove)[0]);
+	print(arrayToRemove);
+	#for array in arrayToRemove:
+	#	data = np.delete(data, array);
+	#print(indexToRemove);
+
+		
+
+	# Remover columnas y convertir arreglo en tipo float
+	#columns = data.shape[1] - 2; # Numero de columnas menos las dos ultimas con valores categoricos
+	#data = data[..., 0:columns]; # Remover columnas con valores categoricos
+	#data = np.asarray(data, dtype=float); # Cambiar el tipo de dato del arreglo a flotante
+	#header = dataset.header;
+	
 
 def readDataSet(filename):
 	#TODO Validar que filename tenga algo; ademas agregar excepciones
 	dataset = pd.read_csv(filename);
 	data = dataset.values;
-	target = np.asarray(list(dataset));
-	return dataWrapper(data, target);
+	header = np.asarray(list(dataset));
+	return dataWrapper(data, header);
 	
 
 class dataWrapper:
-	def __init__(self, data, target):
+	def __init__(self, data, header):
 		self.data = data;
-		self.target = target;
+		self.header = header;
 
 
 
