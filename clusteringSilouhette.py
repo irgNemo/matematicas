4b0858cf3db885e7eda8a0138576147c28580618
Israel Roman Godinez <israel.roman@cucei.udg.mx>
2018-01-11 18:59:51 -0600

    Se agruparon los archivos a procesar en una nueva carpeta llamada Dataset
    Se agregaron archivos con la estructura y formato de nombre de archivo como se va a trabajar
    
    Se corrigio el programa para que procesara todos los archivos con una estructura de nombre especifica, y para todos los casos posibles para tiempo, banda, genero, metica y clase

--- clusteringSilouhette.py
+++ clusteringSilouhette.py
@@ -6,23 +6,53 @@ from sklearn.preprocessing import Imputer
 from sklearn import metrics;
 import pandas as pd;
 import numpy as np;
+from pathlib import Path;
 
 def main():
 
-	dataset = readDataSet("D_C_all_AM.csv");
-	data = dataset.data;
-	extractedData = dropColumnsByHeader(dataset, ['Sujeto','clase']); # Obtenemos la matriz de datos sin los metadatos
-	imputeNaN(extractedData, 1.7976931348623157e+108); # Se asigno este valor de manera arbitraria para que no marcara un error de validacion por valores muy grandes
-	sujetos = extractColumnsByHeader(dataset, ['Sujeto']);# Se obtiene una lista de los sujetos
-	clases = extractColumnsByHeader(dataset, ['clase']);
-	#clusteringAndSilhouette(extractedData, 2, 300, 500, metric='euclidean');
-	silhouette_scores = cohesionBySilhouette(extractedData, clases.flatten(), 'euclidean'); # Se obtiene los scores de silhouette para el conjunto de datos utilizando la etiquetas de clase como sus clusters 
+	tiempos = ['Durante','Despues'];
+	generos = ['H','M'];
+	bandas = ['sf','a','b','d','g','t'];
+	metricas = ['C','E','Gio'];
+	clases = ['AM', 'MB'];
+	directoryPath = "./Datasets/";
+	
+	computeOutliersPerTimeGenderBand(tiempos, generos, bandas, metricas, clases, directoryPath);
+
+
+def computeOutliersPerTimeGenderBand(times, genders, bands, metrics, clases, directoryPath):
+	for time in times:
+		for gender in genders:
+			for band in bands:
+				for metric in metrics:
+					for clase in clases:
+						filename = time + "_" + metric + "_" + band + "_" + gender + "_" + clase;
+						completePathFile = directoryPath + filename + ".csv";
+						my_file = Path(completePathFile);
+						if not my_file.is_file(): # If the file not exists, continues with the following one
+							continue;
+						print(completePathFile);
+						dataset = readDataSet(completePathFile);
+						data = dataset.data;
+						extractedData = dropColumnsByHeader(dataset, ['Sujeto','Clase']); # Obtenemos la matriz de datos sin los metadatos
+						imputeNaN(extractedData, 1.7976931348623157e+108); # Se asigno este valor de manera arbitraria para que no marcara un error de validacion por valores muy grandes
+						sujetos = extractColumnsByHeader(dataset, ['Sujeto']);# Se obtiene una lista de los sujetos
+						clasesnarray = extractColumnsByHeader(dataset, ['Clase']);
+						sujetos_menores_cero = detectOutliers(extractedData, clasesnarray, sujetos, 'euclidean', 0, '<', 1);
+						sujetos_sin_medios = removeRowsByColumnValues(sujetos_menores_cero, 'eq', 0, 'M');
+						print(sujetos_sin_medios.shape);
+
+
+def detectOutliers(data, clases, sujetos, metric, threshold, condition, columnToAnalyse):
+	"""
+	Detect subject with a silhouette score less than threshold
+	"""
+	silhouette_scores = cohesionBySilhouette(data, clases.flatten(), metric); # Se obtiene los scores de silhouette para el conjunto de datos utilizando la etiquetas de clase como sus clusters 
 	silhouette_clase = np.append(clases, silhouette_scores, axis=1); # Se crea una matriz que contiene a los sujetos y sus score de silhouettea
 	silhouette_clases_sujetos = np.append(silhouette_clase, sujetos, axis=1);
-	sujetos_silhouette_menor_cero = filterByColumnValues(silhouette_clases_sujetos, 'lt', 1, 0);
-	print(sujetos_silhouette_menor_cero);
-	print(validateLessEqualPercentage(extractedData, sujetos_silhouette_menor_cero, 40));
-
+	sujetos_silhouette_menor_cero = removeRowsByColumnValues(silhouette_clases_sujetos, condition, columnToAnalyse, threshold);
+	return sujetos_silhouette_menor_cero;
+	
 
 def validateLessEqualPercentage(data, subdata, threshold):
 	"""
@@ -35,13 +65,16 @@ def validateLessEqualPercentage(data, subdata, threshold):
 	return True if (((row_subdata_size*100)/row_data_size) <= threshold) else False;
 
 
-def filterByColumnValues(data, condition, columnToAnalyse, threshold):
+def removeRowsByColumnValues(data, condition, columnToAnalyse, threshold):
 	"""
 	Filter
 	"""
 	indexes = None;
-	if condition == 'lt':
+	if condition == '<':
 		indexes = np.argwhere(data[..., columnToAnalyse] >= threshold);
+		indexes = indexes.flatten();
+	elif condition == 'eq':
+		indexes = np.argwhere(data[..., columnToAnalyse] == threshold);
 		indexes = indexes.flatten();
 	else:
 		print('implement condition');
@@ -55,16 +88,18 @@ def cohesionBySilhouette(data, clases, metric='euclidean'):
 	Measure the cohesion of a group of elements tagged by a clas	
 	return: Array with silhouette scores and cluster label asigned
 	"""
+	rows = data.shape[0];
 	silhouette_scores = metrics.silhouette_samples(data, clases, metric);
-	silhouette_scores = np.reshape(silhouette_scores, (22,1));
+	silhouette_scores = np.reshape(silhouette_scores, (rows,1));
 	return silhouette_scores;	
 
 def clusteringAndSilhouette(data, n_clusters, n_init, max_iter, metric='euclidean'):
+	rows = data.shape[0];
 	kmeans = KMeans(n_clusters=n_clusters, n_init=n_init, max_iter=max_iter);
 	cluster_labels = kmeans.fit_predict(data);
 	silhouette_scores = metrics.silhouette_samples(data, cluster_labels, metric);
-	silhouette_scores = np.reshape(silhouette_scores, (22,1));
-	cluster_labels = np.reshape(cluster_labels, (22,1));
+	silhouette_scores = np.reshape(silhouette_scores, (rows,1));
+	cluster_labels = np.reshape(cluster_labels, (rows,1));
 	silhouette_scores = np.append(silhouette_scores, cluster_labels, axis=1);
 	return	silhouette_scores;
 
@@ -115,7 +150,24 @@ if __name__ == "__main__":
 	main();
 
 
-	# Remover columnas y convertir arreglo en tipo float
-	#columns = data.shape[1] - 2; # Numero de columnas menos las dos ultimas con valores categoricos
-	#data = data[..., 0:columns]; # Remover columnas con valores categoricos
-	#header = dataset.header;
+	"""
+	tuples = filenamesTuples(tiempos, generos, bandas, metricas, clases);
+	for pair in tuples:
+		for filename in pair:
+			completePathFile = directoryPath + filename;
+			my_file = Path(completePathFile);
+			if not my_file.is_file():
+				#print("El archivo " + filename  +  " no fue encontrado");
+				continue;
+			print(completePathFile);
+			dataset = readDataSet(completePathFile);
+			data = dataset.data;
+			extractedData = dropColumnsByHeader(dataset, ['Sujeto','Clase']); # Obtenemos la matriz de datos sin los metadatos
+			imputeNaN(extractedData, 1.7976931348623157e+108); # Se asigno este valor de manera arbitraria para que no marcara un error de validacion por valores muy grandes
+			sujetos = extractColumnsByHeader(dataset, ['Sujeto']);# Se obtiene una lista de los sujetos
+			clases = extractColumnsByHeader(dataset, ['Clase']);
+			sujetos_menores_cero = detectOutliers(extractedData, clases, sujetos, 'euclidean', 0, 'lt', 1);
+			print(sujetos_menores_cero);
+			#if not validateLessEqualPercentage(extractedData, sujetos_menores_cero, 40):
+			#	print("Outlayers mayores al porcentaje en archivo: " + filename);	
+	"""
